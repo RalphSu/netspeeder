a084a09a72ff222ff4e6fe8797983742ac751f3d
das7002 <das7002@a2d133f8-f10d-d5ff-e139-687ae07a72a0>
2011-09-17 23:17:29 +0000

    basic ui and backgroundworker setup (for finding computers later on)

--- netspeeder/MainForm.Designer.cs
+++ netspeeder/MainForm.Designer.cs
@@ -28,19 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.foundListBox = new System.Windows.Forms.ListBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.elipseTimer = new System.Windows.Forms.Timer(this.components);
+            this.computerFinder = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 237);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(339, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(146, 17);
+            this.statusLabel.Text = "Searching for computers...";
+            // 
+            // foundListBox
+            // 
+            this.foundListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.foundListBox.FormattingEnabled = true;
+            this.foundListBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.foundListBox.Location = new System.Drawing.Point(12, 12);
+            this.foundListBox.Name = "foundListBox";
+            this.foundListBox.ScrollAlwaysVisible = true;
+            this.foundListBox.Size = new System.Drawing.Size(142, 212);
+            this.foundListBox.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(160, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(167, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start test with:";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // elipseTimer
+            // 
+            this.elipseTimer.Enabled = true;
+            this.elipseTimer.Interval = 500;
+            this.elipseTimer.Tick += new System.EventHandler(this.elipseTimer_Tick);
+            // 
+            // computerFinder
+            // 
+            this.computerFinder.WorkerReportsProgress = true;
+            this.computerFinder.WorkerSupportsCancellation = true;
+            this.computerFinder.DoWork += new System.ComponentModel.DoWorkEventHandler(this.computerFinder_DoWork);
+            this.computerFinder.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.computerFinder_ProgressChanged);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.ClientSize = new System.Drawing.Size(339, 259);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.foundListBox);
+            this.Controls.Add(this.statusStrip);
+            this.MinimumSize = new System.Drawing.Size(355, 295);
             this.Name = "MainForm";
             this.Text = "LAN Speed Test";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ListBox foundListBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Timer elipseTimer;
+        private System.ComponentModel.BackgroundWorker computerFinder;
     }
 }
--- netspeeder/MainForm.cs
+++ netspeeder/MainForm.cs
@@ -11,9 +11,45 @@ namespace netspeeder
 {
     public partial class MainForm : Form
     {
+        Boolean testing = false;
+        Int32 elipseNum = 1;
         public MainForm()
         {
             InitializeComponent();
         }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            foundListBox.Items.Clear();
+            computerFinder.RunWorkerAsync();
+        }
+
+        private void elipseTimer_Tick(object sender, EventArgs e)
+        {
+            if (!testing)
+            {
+                statusLabel.Text = "Searching for computers";
+                statusLabel.Text += new String('.', elipseNum);
+                elipseNum++;
+                if (elipseNum > 3)
+                {
+                    elipseNum = 0;
+                }
+            }
+        }
+
+        private void computerFinder_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                computerFinder.ReportProgress(-1, "ComputerName (127.0.0.1)");
+                System.Threading.Thread.Sleep(400);
+            }
+        }
+
+        private void computerFinder_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            foundListBox.Items.Add(e.UserState);
+        }
+
     }
 }
